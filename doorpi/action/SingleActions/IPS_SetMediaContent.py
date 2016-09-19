3157127685826a6965c272ee4be3683837c6e1c7
Hermann <doorpi1@gmail.com>
2016-09-19 23:07:41 +0200

    neue SingleActions:
    ipsprc_set_media_content
    
    wegen Änderung der Befehlsreferenz Symcon 4.0

--- doorpi/action/SingleActions/IPS_SetMediaContent.py
+++ doorpi/action/SingleActions/IPS_SetMediaContent.py
@@ -8,12 +8,17 @@ logger.debug("%s loaded", __name__)
 import doorpi
 import requests
 import json
+import base64
+from io import BytesIO
+from time import sleep
+from picamera import PiCamera
+from PIL import Image
 from requests.auth import HTTPBasicAuth
 from doorpi.action.base import SingleAction
 
 CONFIG = doorpi.DoorPi().config
 
-def ips_rpc_fire(method, *parameters):
+def IPS(method, *parameters):
     url = CONFIG.get('IP-Symcon', 'server')
     auth=HTTPBasicAuth(CONFIG.get('IP-Symcon', 'username'), CONFIG.get('IP-Symcon', 'password'))
     headers = {'content-type': 'application/json'}
@@ -26,33 +31,32 @@ def ips_rpc_fire(method, *parameters):
     }
     return requests.post(url, auth=auth, data=json.dumps(payload), headers=headers)
 
-def ips_rpc_check_variable_exists(key):
-    response = ips_rpc_fire('IPS_VariableExists', key)
+def IPS_MediaExists(key):
+    response = IPS('IPS_MediaExists', key)
     return response.json()['result']
 
-def ips_rpc_get_variable_type(key):
-    response = ips_rpc_fire('IPS_GetVariable', key)
-    return response.json()['result']['VariableType']
+def IPS_GetMedia(key):
+    response = IPS('IPS_GetMedia', key)
+    return response.json()['result']['MediaType']
 
-def ips_rpc_get_variable_value(key):
-    response = ips_rpc_fire('GetValue', key)
-    return response.json()['result']
-
-def ips_rpc_set_media_content(key, value):
+def IPS_SetMediaContent(key):
     try:
-        if ips_rpc_check_variable_exists(key) is not True: raise Exception("var %s doesn't exist", key)
-        type = ips_rpc_get_variable_type(key)
+        if IPS_MediaExists(key) is not True: raise Exception("var %s doesn't exist", key)
+        type = IPS_GetMedia(key)
         if type is None: raise Exception("type of var %s couldn't find", key)
         # http://www.ip-symcon.de/service/dokumentation/befehlsreferenz/variablenverwaltung/ips-getvariable/
         # Variablentyp (0: Boolean, 1: Integer, 2: Float, 3: String)
-        elif type == 0:
-            if value.lower() in ['true', 'yes', '1']: value = True
-            else: value = False
-        elif type == 1: value = int(value)
-        elif type == 2: value = float(value)
-        elif type == 3: value = str(value)
-        else: value = str(
-        ips_rpc_fire('IPS_SetMediaContent', key, value)
+        elif type == 1:
+            stream = BytesIO()
+            camera = PiCamera()
+            camera.start_preview()
+            sleep(2)
+            camera.capture(stream, format='jpeg')
+            # "Rewind" the stream to the beginning so we can read its content
+            stream.seek(0)
+            value = base64.b64encode(str(Image.open(stream)))
+            IPS('IPS_SetMediaContent', key, value)
+            camera.close()
     except Exception as ex:
         logger.exception("couldn't send IpsRpc (%s)", ex)
         return False
@@ -60,12 +64,11 @@ def ips_rpc_set_media_content(key, value):
 
 def get(parameters):
     parameter_list = parameters.split(',')
-    if len(parameter_list) is not 2: return None
+    if len(parameter_list) is not 1: return None
 
     key = int(parameter_list[0])
-    value = parameter_list[1]
 
-    return IpsRpcSetMediaContentAction(ips_rpc_set_media_content, key, value)
+    return IPS_SetMediaContentAction(IPS_SetMediaContent, key)
 
-class IpsRpcSetMediaContentAction(SingleAction):
+class IPS_SetMediaContentAction(SingleAction):
     pass
