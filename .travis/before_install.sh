d1f65f872941e46d3896406a1df899b039337e1a
Andreas Loeffler <al@exitzero.de>
2019-09-04 20:13:59 +0200

    try get travis running

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,7 +3,9 @@
 set -e # Exit immediately if a command exits with a non-zero status.
 set -x # Print commands and their arguments as they are executed.
 
-pip install --upgrade pip
+#pip install --upgrade pip
+python -m pip install pip==9.0.3
+
 pip --version
 #pip uninstall -y setuptools
 #sudo rm -f /usr/local/lib/python2.7/dist-packages/setuptools*.egg
